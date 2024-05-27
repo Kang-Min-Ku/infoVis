d260989f58b5eb8a9e4887ddebec827e00e4d312
Kang Min Ku <38034926+Kang-Min-Ku@users.noreply.github.com>
2024-05-27 15:17:55 +0900

    src code until lolipop chart

---
+++ vis/components/heatmap.js
@@ -0,0 +1,15 @@
+class LinkedHeatMap{
+    margin = {
+        top: 10, right: 10, bottom: 50, left: 20
+    }
+
+    constructor(svg, width=1200, height=600){
+        this.svg = svg;
+        this.width = width;
+        this.height = height;
+    }
+
+    initialize(){
+        
+    }
+}
